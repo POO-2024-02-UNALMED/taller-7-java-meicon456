cc9c4233a1638bdcf4a2f1f63b2ef4f714597e76
meicon456 <jocastrore@unal.edu.co>
2025-01-14 08:26:08 -0500

    Update Alfabeto.java

--- src/main/java/comunicacion/Alfabeto.java
+++ src/main/java/comunicacion/Alfabeto.java
@@ -1,42 +1,44 @@
 package comunicacion;
 
-import java.util.List;
-import java.util.Arrays;
-
 public class Alfabeto extends Pictograma {
-    private static List<String> letras = Arrays.asList(
-        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-    );
+    private String origen;
+    private String[] letras;
     private String interpretacion;
 
-    public Alfabeto(String origen, List<String> letras, String interpretacion) {
-        super(origen);
-        this.letras = letras; 
+    public Alfabeto(String origen, String[] letras, String interpretacion) {
+        this.origen = origen;
+        this.letras = letras;
         this.interpretacion = interpretacion;
     }
 
-    public static List<String> getLetras() {
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public String[] getLetras() {
         return letras;
     }
 
-    public static void setLetras(List<String> letras) {
-        Alfabeto.letras = letras;
+    public void setLetras(String[] letras) {
+        this.letras = letras;
     }
 
-    public String getInterpretacion() {
+    @Override
+    public String interpretacion() {
         return interpretacion;
     }
 
-    public void setInterpretacion(String interpretacion) {
-        this.interpretacion = interpretacion;
-    }
-
-    public int cantidadLetras() {
-        return letras.size();
+    @Override
+    public int palabrasTotales(int palabrasPagina) {
+        return letras.length * palabrasPagina;
     }
 
     @Override
     public String toString() {
-        return String.join(", ", letras);
+        return getOrigen() + "\n" + String.join(", ", letras);
     }
 }
