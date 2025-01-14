bb5490e95b9b6af087954fd8a2801ce0f2b2fd0a
meicon456 <jocastrore@unal.edu.co>
2025-01-14 08:46:27 -0500

    Update Tesis.java

--- src/main/java/comunicacion/Tesis.java
+++ src/main/java/comunicacion/Tesis.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 public class Tesis extends Escrito {
     private String idea;
-    private List<String> argumentos;
+    private String[] argumentos;
     private String conclusion;
     private String referencias;
     private String interpretacion;
 
-    public Tesis(String origen, String titulo, String autor, int paginas, String idea, List<String> argumentos, String conclusion, String referencias, String interpretacion) {
+    public Tesis(String origen, String titulo, String autor, int paginas, String idea, String[] argumentos, String conclusion, String referencias, String interpretacion) {
         super(origen, titulo, autor, paginas);
         this.idea = idea;
         this.argumentos = argumentos;
@@ -26,11 +26,11 @@ public class Tesis extends Escrito {
         this.idea = idea;
     }
 
-    public List<String> getArgumentos() {
+    public String[] getArgumentos() {  
         return argumentos;
     }
 
-    public void setArgumentos(List<String> argumentos) {
+    public void setArgumentos(String[] argumentos) {  
         this.argumentos = argumentos;
     }
 
